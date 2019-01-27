2240e58bc8bf1352e40b6b31562f8efd8d21cdd9
JS <jsoto3000@outlook.com>
2019-01-27 18:58:09 -0500

    fixed draw error

--- js/index.js
+++ js/index.js
@@ -4,17 +4,13 @@
 // btwn 0 and .9999999999999999
 // never reaches 1
 
-var randomNumber1 = Math.random();
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-randomNumber1 = randomNumber1 * 6;
+dice1 = "images/dice" + randomNumber1 + ".png"; // to calc n btwn 1 & 6
 
-dice1 = "images/dice" + (Math.floor(randomNumber1) + 1) + ".png"; // to calc n btwn 1 & 6
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomNumber2 = Math.random();
-
-randomNumber2 = randomNumber2 * 6;
-
-dice2 = "images/dice" + (Math.floor(randomNumber2) + 1) + ".png"; // to calc n btwn 1 & 6
+dice2 = "images/dice" + randomNumber2 + ".png"; // to calc n btwn 1 & 6
 
 // console.log test
 // var img1 = document.getElementsByClassName("img1");
@@ -25,3 +21,20 @@ img1[0].setAttribute("src", dice1);
 
 var img2 = document.getElementsByClassName("img2");
 img2[0].setAttribute("src", dice2);
+
+// if else sample
+/*
+if (condition) {
+  //  block of code to be executed if the condition is true
+} else {
+  //  block of code to be executed if the condition is false
+}
+*/
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "It's a Draw!";
+}
